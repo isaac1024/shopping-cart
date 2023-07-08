d8bf1a7eb248763373262618a435ccfaa6392b09
Isaac Abreu Rodríguez <caasi1024@gmail.com>
2023-07-08 21:19:26 +0200

    Implement use case to cart product remover controller

--- api/src/ShoppingCart/Cart/Infrastructure/Controller/CartProductRemoverController.php
+++ api/src/ShoppingCart/Cart/Infrastructure/Controller/CartProductRemoverController.php
@@ -2,6 +2,9 @@
 
 namespace ShoppingCart\Cart\Infrastructure\Controller;
 
+use ShoppingCart\Cart\Application\CartProductRemoverCommand;
+use ShoppingCart\Cart\Domain\CartIdException;
+use ShoppingCart\Cart\Domain\NotFoundCartException;
 use ShoppingCart\Cart\Infrastructure\Request\CartProductRemoverRequest;
 use ShoppingCart\Shared\Infrastructure\ApiController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,11 +13,16 @@ final readonly class CartProductRemoverController extends ApiController
 {
     public function __invoke(CartProductRemoverRequest $request, string $cartId): JsonResponse
     {
+        $this->commandBus->dispatch(new CartProductRemoverCommand($cartId, $request->productId));
+
         return new JsonResponse(null, 204);
     }
 
     protected function mapExceptions(): array
     {
-        return [];
+        return [
+            NotFoundCartException::class => 404,
+            CartIdException::class => 400,
+        ];
     }
 }
