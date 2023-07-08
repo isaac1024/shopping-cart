413f070565db1de55e63f64c91e349993fbb08ba
Isaac Abreu Rodríguez <caasi1024@gmail.com>
2023-07-08 16:44:05 +0200

    Implement use case on cart finder controller

--- api/src/ShoppingCart/Cart/Infrastructure/Controller/CartFinderController.php
+++ api/src/ShoppingCart/Cart/Infrastructure/Controller/CartFinderController.php
@@ -2,6 +2,7 @@
 
 namespace ShoppingCart\Cart\Infrastructure\Controller;
 
+use ShoppingCart\Cart\Application\CartFinderQuery;
 use ShoppingCart\Cart\Domain\NotFoundCartException;
 use ShoppingCart\Shared\Infrastructure\ApiController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,7 +12,9 @@ final readonly class CartFinderController extends ApiController
 {
     public function __invoke(string $cartId): JsonResponse
     {
-        return new JsonResponse([], Response::HTTP_OK);
+        $cartResponse = $this->queryBus->ask(new CartFinderQuery($cartId));
+
+        return new JsonResponse($cartResponse, Response::HTTP_OK);
     }
 
     protected function mapExceptions(): array
--- api/tests/Cart/Infrastructure/Controller/CartFinderControllerTest.php
+++ api/tests/Cart/Infrastructure/Controller/CartFinderControllerTest.php
@@ -3,6 +3,7 @@
 namespace ShoppingCart\Tests\Cart\Infrastructure\Controller;
 
 use ShoppingCart\Cart\Domain\CartRepository;
+use ShoppingCart\Shared\Domain\Models\UuidUtils;
 use ShoppingCart\Tests\Cart\Domain\CartObjectMother;
 use ShoppingCart\Tests\Shared\Infrastructure\PhpUnit\AcceptanceTestCase;
 
@@ -15,5 +16,10 @@ class CartFinderControllerTest extends AcceptanceTestCase
 
         $this->json('GET', sprintf("/carts/%s", $cart->getCartId()));
         self::assertResponseStatusCodeSame(200);
+    }
+    public function testNotFoundACart()
+    {
+        $this->json('GET', sprintf("/carts/%s", UuidUtils::random()));
+        self::assertResponseStatusCodeSame(404);
     }
 }
